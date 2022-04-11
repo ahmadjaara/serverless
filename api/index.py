7fa8707d178c532ea401b845a375facdef3082df
ahmadjaara <ahmadjaara98@yahoo.com>
2022-04-11 18:56:25 +0300

    new ahmad

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-from datetime import datetime
+
 
 class handler(BaseHTTPRequestHandler):
     
@@ -17,5 +17,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()  
-        self.wfile.write(message.encode(),str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        self.wfile.write(message.encode())
         return
