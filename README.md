6a098117a628d0c1a628c3a97ab7c912c6c7a1b3
ahmadjaara <ahmadjaara98@yahoo.com>
2022-04-11 18:53:45 +0300

    ahmad1

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Create a serverless function following Vercel’s steps.
 - The function should handle a GET http request.
 - The function should accomplish some useful task.
 - The function should parse the url path’s query string.
+
+manual test
+creating a function that handel query string that will take a name and print the result "Hi, stranger" if there is no name provided if not it will print the name
+
+with name
+
+<https://serverless-swart-zeta.vercel.app/api/index?name=ahmad>
+
+without name
+
+<https://serverless-swart-zeta.vercel.app/api/index>
+
+
--- api/index.py
+++ api/index.py
@@ -1,19 +1,21 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         s = self.path
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()        
+              
         
         if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
+            message = "Hi, " + dic["name"] + "!"
         else:
-            message = "Hello, stranger!"        
-        
-        self.wfile.write(message.encode())
+            message = "Hi, stranger!"  
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()  
+        self.wfile.write(message.encode(),str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
         return
