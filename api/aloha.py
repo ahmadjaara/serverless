9de12e82bdc75fc4b39f38b19e921928b59194bf
ahmadjaara <ahmadjaara98@yahoo.com>
2022-04-11 17:57:55 +0300

    meduim

--- api/aloha.py
+++ api/aloha.py
@@ -8,16 +8,16 @@ class handle(BaseHTTPRequestHandler):
         url_components=parse.urlsplit(s)
         query_string_list=parse.parse_qsl(url_components.query)
         dic=dict(query_string_list)
-
-        name=dic.get("name")
-
-        if name:
-            message = f"Aloha {name}"
-        else:
-            message ="Aloha stranger"
-        message+=f"\nGreetings from Python version {platform.python_version()}"
+        
         self.send_response(200)
         self.send_header('content-type','text/plain')
         self.end_headers()
 
+        if "name" in dic:
+            message = "Aloha,"+ dic ["name"]+"!"
+        else:
+            message ="Aloha, stranger"
+        
+        
         self.wfile.write(message.encode())
+        return
